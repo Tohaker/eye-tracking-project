ab2a7fc562212e092e1c1610dbb4e28335747922
Miles Bardon <milesbardon@gmail.com>
2017-11-15 18:43:16 +0000

    Added first testing program. Uses an 8x8 grid to show gaze, and when Space is held down demonstrates the change between Activation Mode and Tentative Activation Mode.

---
+++ Wpf_Extended/Interaction_Wpf_101/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using Tobii.Interaction.Wpf;
+
+namespace Interaction_Wpf_101
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int _amountOfActivations = 0;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                ((App)Application.Current).Host.Commands.Input.SendActivationModeOn();
+            }
+        }
+
+        private void MainWindow_OnPreviewKeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                ((App)Application.Current).Host.Commands.Input.SendActivation();
+                ((App)Application.Current).Host.Commands.Input.SendActivationModeOff();
+            }
+        }
+
+        private void Grid_OnActivated(object sender, ActivationRoutedEventArgs e)
+        {
+            _amountOfActivations++;
+            NumberOfActivations.Text = _amountOfActivations.ToString();
+        }
+    }
+}
