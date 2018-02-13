8547883ba06b0b1d4af2a740649fec20f23816ca
Tohaker <33665681+Tohaker@users.noreply.github.com>
2018-02-13 15:29:32 +0000

    Update VehicleControl.cs
    Began adding in methods to link message and communicator classes together.

--- VehicleApp/VehicleApp/VehicleControl.cs
+++ VehicleApp/VehicleApp/VehicleControl.cs
@@ -6,8 +6,40 @@ using System.Threading.Tasks;
 
 namespace VehicleApp
 {
+    
+    #region Private Variables
+    private Message message;
+    private Communicator connection;
+    #endregion
+    
+    #region Public Declarations
+    #endregion
+    
     class VehicleControl
     {
-
+        public VehicleControl(string COM)
+        {
+            if (message == null)
+            {
+                message = new Message();
+            }
+            
+            if (connection == null)
+            {
+                connection = new Communicator(COM); // Needs connection info - collect from construction of Vehicle Control?
+            }
+        }
+        
+        public SendMessage(int command, int data = 0)
+        {
+            string msg;
+            
+            if (data == 0)
+                msg = message.PackageMessage(command);
+            else
+                msg = message.PackageMessage(command, data);
+            
+            // TODO: Create "IsOpen" checked for communicator class
+        }
     }
 }
